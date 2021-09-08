093b6ea9ed273da9ef02ba14fe0ea18e0bbc277b
devroy <suhuyn19@naver.com>
2021-09-08 15:07:40 +0900

    feat: guard 및 resolve 파일 추가

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  UserLogin: boolean = true;
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    console.log('AuthGuard Called');
+    if (this.UserLogin) {
+      return true;
+    }
+
+    this.router.navigate(['/users']);
+    return false;
+  }
+}
